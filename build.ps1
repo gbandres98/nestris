1a3ab0d70170191489802102a979a71c86418e00
Guillermo Bandrés <mail@gbandres.com>
2025-07-27 18:50:22 +0200

    change file extension

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 Remove-Item ./build/nestris.o -erroraction 'silentlycontinue'
-ca65 nestris.asm -o ./build/nestris.o --debug-info
+ca65 nestris.s -o ./build/nestris.o --debug-info
 Remove-Item ./build/nestris_bck.nes -erroraction 'silentlycontinue'
 Move-Item ./build/nestris.nes ./build/nestris_bck.nes -erroraction 'silentlycontinue'
 Remove-Item ./build/nestris.dbg -erroraction 'silentlycontinue'
